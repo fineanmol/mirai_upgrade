03fc6db757491ad801588403c0f91a9921623ca7
ANMOL AGARWAL <24794539+fineanmol@users.noreply.github.com>
2020-08-17 23:53:43 +0530

    ConfirmationActivity basic layout ready

---
+++ app/src/main/java/com/tragicbytes/midi/activity/ConfirmationActivity.kt
@@ -0,0 +1,14 @@
+package com.tragicbytes.midi.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.tragicbytes.midi.R
+import kotlinx.android.synthetic.main.toolbar.*
+
+class ConfirmationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_confirmation)
+
+    }
+}
