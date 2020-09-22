239cb3b716224100b8c3f2f4699345eea220f075
Nikhil Nishad <nikhilnishad279@gmail.com>
2020-09-22 16:09:56 +0530

    notification server switched to test server
    Signed-off-by: Nikhil Kr. Nishad <nikhilnishad279@gmail.com>

--- app/src/main/java/com/tragicbytes/midi/utils/Constants.kt
+++ app/src/main/java/com/tragicbytes/midi/utils/Constants.kt
@@ -23,7 +23,7 @@ object Constants {
          const val consumerSecret = "z5RTcjT0NseqCYbfTQXaSwSNC054HLp0SGGhqbMKlpWWtOTH"
          const val token = "gN3kZdeh2Yyh6Ntdvip4Mokf"
          const val tokenSecret = "EhhwL3Ta6GSevnG6aKd4pCdp2X3rosIivzubXmZxSLkByi31"
-        private const  val NOTIFICATION_DEFAULT_URL = "https://notification-server-290108.uc.r.appspot.com/"
+        private const  val NOTIFICATION_DEFAULT_URL = "https://mv-dev-fcm-notification-server.herokuapp.com/"
         const  val NOTIFICATION_BASE_URL= NOTIFICATION_DEFAULT_URL+"notify/"
     }
 
